19fd72f198aa01116f7dd851e3af8703fee92e73
Parikshit Machwe <pmachwe@gmail.com>
2023-09-05 21:06:17 +0530

    setting up telescope and related utils

---
+++ lua/plugins/telescope_extras.lua
@@ -0,0 +1,30 @@
+return {
+    -- FZF
+    {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+        dependencies = { "nvim-telescope/telescope.nvim" },
+        --config = function()
+            --require('telescope').load_extension('fzf')
+        --end,
+    },
+
+    -- P4 files
+    {
+        'Badhi/vim-p4-files',
+        dependencies = { "nvim-telescope/telescope.nvim" },
+        config = function()
+            require('telescope').load_extension('vim_p4_files')
+        end,
+    },
+
+    -- Yanked text
+    {
+        "AckslD/nvim-neoclip.lua",
+        dependencies = { "nvim-telescope/telescope.nvim" },
+        config = function()
+            require('neoclip').setup()
+            require('telescope').load_extension('neoclip')
+        end,
+    },
+}
