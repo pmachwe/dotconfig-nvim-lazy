4068b9bc4cddd6c55e46caf11a4b285091fcf748
Parikshit Machwe <pmachwe@gmail.com>
2023-12-25 14:34:29 +0530

    Adding nvim-surround to maniuplate paranthesis and other enclosing characters.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -103,4 +103,16 @@ return {
             vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
         end
     },
+
+    -- Surround
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    },
 }
