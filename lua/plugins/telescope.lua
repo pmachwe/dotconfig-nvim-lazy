b5435415e2b106970db30857a716a80c2ebfe654
Parikshit Machwe <pmachwe@gmail.com>
2024-02-04 22:16:10 +0530

    Moving back to telescope from fzf.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.2',
-    enabled = false,
+    -- enabled = false,
     dependencies = { 'nvim-lua/plenary.nvim' },
     cmd = "Telescope",
     keys = {
