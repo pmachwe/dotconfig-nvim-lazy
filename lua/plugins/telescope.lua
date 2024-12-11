f30ae1ba07e20a65842c2feda38bc56a42481be7
Parikshit Machwe <pmachwe@gmail.com>
2024-12-11 20:47:41 +0530

    More updates to snacks and telescope ivy.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,26 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.2',
+    'nvim-telescope/telescope.nvim', 
     -- enabled = false,
     dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function() 
+        require("telescope").setup {
+            pickers = {
+                find_files = {
+                    theme = "ivy",
+                },
+            },
+            extensions = {
+                fzf = {
+                    fuzzy = true, -- false will only do exact matching
+                    override_generic_sorter = true, -- override the generic sorter
+                    override_file_sorter = true, -- override the file sorter
+                    case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+                    -- the default case_mode is "smart_case"
+                }
+            },
+        }
+        require("telescope").load_extension("fzf")
+    end,
     cmd = "Telescope",
     keys = {
         { "<leader>,", "<cmd>Telescope buffers show_all_buffers=true<cr>", desc = "Switch Buffer" },
